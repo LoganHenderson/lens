61eb6fe1318cb703d18d50a0ec07720e1804e7ca
Logan Henderson <loganjhenderson21@gmail.com>
2022-01-09 19:37:12 -0500

    slashing queries

---
+++ client/slashing.go
@@ -0,0 +1,39 @@
+package client
+
+import (
+	"context"
+
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	querytypes "github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/cosmos/cosmos-sdk/x/slashing/types"
+)
+
+func (cc *ChainClient) QuerySlashingSigningInfo(publicKey cryptotypes.PubKey) (*types.QuerySigningInfoResponse, error) {
+	cc.EncodeBech32ConsPub(sdk.addressfrom)
+	params := &types.QuerySigningInfoRequest{ConsAddress: sdk.ConsAddress(publicKey.Address()).String()}
+	res, err := types.NewQueryClient(cc).SigningInfo(context.Background(), params)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (cc *ChainClient) QuerySlashingSigningInfos(pageReq *querytypes.PageRequest) (*types.QuerySigningInfosResponse, error) {
+	res, err := types.NewQueryClient(cc).SigningInfos(context.Background(),
+		&types.QuerySigningInfosRequest{Pagination: pageReq})
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (cc *ChainClient) QuerySlashingParams() (*types.QueryParamsResponse, error) {
+	res, err := types.NewQueryClient(cc).Params(context.Background(), &types.QueryParamsRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
